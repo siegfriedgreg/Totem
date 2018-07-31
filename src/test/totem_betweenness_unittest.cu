738a23fee1f920f3d4274e2913bd6c8045a2d932
Greg Siegfried <40606199+siegfriedgreg@users.noreply.github.com>
2018-07-31 15:51:00 -0400

    Update totem_betweenness_unittest.cu

--- src/test/totem_betweenness_unittest.cu
+++ src/test/totem_betweenness_unittest.cu
@@ -164,6 +164,6 @@ INSTANTIATE_TEST_CASE_P(BetweennessGPUAndCPUTest, BetweennessCentralityTest,
 // From Google documentation:
 // Google Test may not support value-parameterized tests with some
 // compilers. This dummy test keeps gtest_main linked in.
-TEST_P(DummyTest, ValueParameterizedTestsAreNotSupportedOnThisPlatform) {}
+// TEST_P(DummyTest, ValueParameterizedTestsAreNotSupportedOnThisPlatform) {}
 
 #endif  // GTEST_HAS_PARAM_TEST
