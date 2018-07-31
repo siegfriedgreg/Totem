87c334d13e5713cef08725a500825fe6b1dfd2e9
Greg Siegfried <40606199+siegfriedgreg@users.noreply.github.com>
2018-07-31 15:52:00 -0400

    Update totem_clustering_unittest.cu

--- src/test/totem_clustering_unittest.cu
+++ src/test/totem_clustering_unittest.cu
@@ -193,6 +193,6 @@ INSTANTIATE_TEST_CASE_P(ClusteringCoefficientGPUandCPUTest,
 // From Google documentation:
 // Google Test may not support value-parameterized tests with some
 // compilers. This dummy test keeps gtest_main linked in.
-TEST_P(DummyTest, ValueParameterizedTestsAreNotSupportedOnThisPlatform) {}
+// TEST_P(DummyTest, ValueParameterizedTestsAreNotSupportedOnThisPlatform) {}
 
 #endif  // GTEST_HAS_PARAM_TEST
