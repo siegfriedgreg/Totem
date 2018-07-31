70aad2cce3db43d13ad50b8b2ff24e67d71b3d37
Greg Siegfried <40606199+siegfriedgreg@users.noreply.github.com>
2018-07-31 15:54:39 -0400

    Update totem_maxflow_unittest.cu

--- src/test/totem_maxflow_unittest.cu
+++ src/test/totem_maxflow_unittest.cu
@@ -150,6 +150,6 @@ INSTANTIATE_TEST_CASE_P(MaxFlowGPUAndCPUTest, MaxFlowTest,
 // From Google documentation:
 // Google Test may not support value-parameterized tests with some
 // compilers. This dummy test keeps gtest_main linked in.
-TEST_P(DummyTest, ValueParameterizedTestsAreNotSupportedOnThisPlatform) {}
+// TEST_P(DummyTest, ValueParameterizedTestsAreNotSupportedOnThisPlatform) {}
 
 #endif  // GTEST_HAS_PARAM_TEST
